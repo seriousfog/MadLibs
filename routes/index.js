06acdf5d048a226a9b8720952b57ab7b9843db20
t9uo <t9uo@fresnou.org>
2023-09-11 09:28:15 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index',);
 });
 
+router.post( '/story', function(req, res){
+  let body = req.body;
+  let newStory = getStory(body);
+  res.render ('story',{
+    newStory: newStory
+  });
+})
+
+
 module.exports = router;
+
+function getStory(formData) {
+  if (formData.storyChoice === "1") {
+    return generateStory1(formData);
+  } else if (formData.storyChoice === "2") {
+    return generateStory2(formData);
+  } else if (formData.storyChoice === "3") {
+    return generateStory3(formData);
+  } else {
+    return "invalid";
+  }
+}
+
+function generateStory1(formData) {
+  return `I went and I found my friend Jonathan's  ${formData.noun1}. The ${formData.adjective}
+   pumpkins started to ${formData.verb1}!`
+}
+
+function generateStory2(formData) {
+  return `Twas a cold and wintery ${formData.noun1}. The ${formData.adjective}
+   pumpkins started to ${formData.verb1}!`
+}
+
+function generateStory3(formData) {
+  return `Twas a rainy and cold ${formData.noun1}. The ${formData.adjective}
+   pumpkins started to ${formData.verb1}!`
+}
