19b752687bfb5e73337e71f0f7c734c6b5ba2513
t9uo <t9uo@fresnou.org>
2023-09-12 09:27:43 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -24,22 +24,34 @@ function getStory(formData) {
     return generateStory2(formData);
   } else if (formData.storyChoice === "3") {
     return generateStory3(formData);
+  } else if (formData.storyChoice === "4") {
+    return generateStory4(formData);
   } else {
     return "invalid";
   }
 }
 
 function generateStory1(formData) {
-  return `I went and I found my friend Jonathan's  ${formData.noun1}. The ${formData.adjective}
-   pumpkins started to ${formData.verb1}!`
+  return `I went and I found my friend ${formData.name1}'s  ${formData.noun1}. ${formData.name1}'s ${formData.noun1}
+   was bright and ${formData.adjective1}. When I found it, it seemed like it started to ${formData.verb1}. I was so
+  scared and quickly ran away from the ${formData.noun1}. I looked back and ${formData.name1}'s ${formData.noun1}
+   seemed liked it was ${formData.verb2} towards me. I was very frightened and hid from ${formData.name1}'s ${formData.noun1}.`
 }
 
 function generateStory2(formData) {
-  return `Twas a cold and wintery ${formData.noun1}. The ${formData.adjective}
-   pumpkins started to ${formData.verb1}!`
+  return `My friend ${formData.name1} had a very expensive ${formData.noun1} that costed over 100,000$.
+  It was very ${formData.adjective1} and seemed like it might've been worth the price. ${formData.name1}'s 
+  ${formData.noun1} started to ${formData.verb1} was mostl likely broken after 24 hours of being bought.
+  The ${formData.noun1} also was ${formData.verb2} which made me very uncomfortable.`
 }
 
 function generateStory3(formData) {
-  return `Twas a rainy and cold ${formData.noun1}. The ${formData.adjective}
-   pumpkins started to ${formData.verb1}!`
+  return `*BOOM* *BOOM* *BOOM* *BOOM* *BOOM* Many bombs were dropped on us by the ${formData.adjective1}
+  ${formData.noun1}. My name was ${formData.name1}, the last hope of humanity. I tried stopping the 
+  ${formData.noun1}, but I couldn't because it would ${formData.verb1} everytime I got close. When I caught him
+  alone in a corner, he somehow managed to escape by ${formData.verb2}. I was frustrated of how weak I was.`
+}
+
+function generateStory4 (formData) {
+
 }
